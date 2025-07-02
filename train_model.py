ef1002cca5ad4bd52ca7d9fec072faba8f2a761e
sk007-ui <sathwik466@GMAIL.COM>
2025-07-02 07:55:40 +0530

    joblib

--- train_model.py
+++ train_model.py
@@ -1,9 +1,12 @@
 from sklearn.datasets import load_iris
 from sklearn.linear_model import LogisticRegression
+import joblib
 
 X, y = load_iris(return_X_y=True)
 
 model = LogisticRegression(max_iter=200)
 model.fit(X, y)
 
-print("Model trained successfully")
+joblib.dump(model, 'iris_model.joblib')
+
+print("Model trained and saved as iris_model.joblib")
